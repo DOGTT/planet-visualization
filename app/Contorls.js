5219f943905b9ce47e140bf4847e53fc31af4a86
DOGTT <bolitt@hotmail.com>
2017-06-25 16:14:37 +0800

    update

--- app/Contorls.js
+++ app/Contorls.js
@@ -33,7 +33,8 @@ var Contorl = function(){
             latlon.innerHTML= '';
             //console.log(mouse);
         }
-
+        //地图加载
+        mapLoad();
         var gui = new dat.GUI();
         var textureGui = gui.addFolder( 'texture' );
         textureGui.add( planet.params, 'lolaLinesShow' );
@@ -44,12 +45,16 @@ var Contorl = function(){
         gui.add( paramC, 'RayMarching' ).onChange(paramOnChange);
         //planet.setParams(params);
         gui.open();
-        var cdsac = new THREE.BoxGeometry(0.8,0.6,0.1 );
+        var cdsac = new THREE.BoxGeometry(0.01,0.01,0.01 );
 		var cdaca = new THREE.MeshBasicMaterial( {wireframe:true} );
-        var cs = new THREE.Mesh(cdsac,cdaca);
-        planet.addMesh(cs,'test',117.5,22.35,0.2);
-        cs.material = new THREE.MeshBasicMaterial( {wireframe:true,color:0xffffff} );
-         mapLoad();
+        var lons = 100,lats = 0;
+        for(var i = 0;i<50;i++){
+            for(var j = 0;j<40;j++){
+            var cs = new THREE.Mesh(cdsac,cdaca);
+                planet.addMesh(cs,'test',lons + j*0.8,lats + i*0.8,0.2);
+            }
+        }
+         
     }
     function mapLoad(){
         var filename = "china-provinces.json";//world-50m
