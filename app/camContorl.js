49fb345cf4c68495cf73a6e25b20284431167bc3
DOGTT <bolitt@hotmail.com>
2017-07-06 23:52:30 +0800

    m

--- app/camContorl.js
+++ app/camContorl.js
@@ -1,3 +1,17 @@
 /**
  * start
  */
+(function (global,factory){
+    typeof exports === 'object' && typeof module!=='undefined'?module.exports=factory():
+    typeof define === 'function' && define.amd?define(factory):(global.PlanetCamearContorl=factory());   
+}(this,(function(){
+    'use strict';
+/**
+ * @author tide_h
+ * loading PlanetCamearContorl control
+ */
+var PlanetCamearContorl = function(){
+
+}
+return PlanetCamearContorl;
+})));
