3bbe5b793cdfccb95799a119d6f5f31ceec8f406
DOGTT <bolitt@hotmail.com>
2017-07-10 23:25:14 +0800

    change name

--- app/viewControler.js
+++ app/viewControler.js
@@ -3,14 +3,14 @@
  */
 (function (global,factory){
     typeof exports === 'object' && typeof module!=='undefined'?module.exports=factory():
-    typeof define === 'function' && define.amd?define(factory):(global.PlanetCamearContorl=factory());   
+    typeof define === 'function' && define.amd?define(factory):(global.viewControler=factory());   
 }(this,(function(){
     'use strict';
 /**
  * @author tide_h
  * loading PlanetCamearContorl control
  */
-var PlanetCamearContorl = function(){
+var viewControler = function(){
 
 }
 return PlanetCamearContorl;
