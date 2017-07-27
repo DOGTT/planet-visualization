9e1c1401639232151093644fc4e4714f095fd7f7
DOGTT <bolitt@hotmail.com>
2017-07-27 23:11:36 +0800

    m

--- app/Contorls.js
+++ app/Contorls.js
@@ -3,61 +3,70 @@
  * Contorl part
  * need update
  */
-var Contorl = function(){
+var Contorl = function() {
     'use strict';
     var planet = new Planet({
-        cloudShow:false,
-        textShow:false});
+        cloudShow: false,
+        textShow: false
+    });
     var mouse = new THREE.Vector2();
     var paramC = {
-            RayMarching:false
+        RayMarching: false
     };
     var objName = {
-        rayMarch:'raymarching'
+        rayMarch: 'raymarching'
     }
     init();
 
-    function init(){
+    function init() {
 
         var latlon = document.createElement('div');
         latlon.style.cssText = 'position:fixed;bottom:2.5%;right:1%;opacity:0.9;z-index:10000';
         planet.container.appendChild(latlon);
 
-        document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-        function onDocumentMouseMove( event ) {
+        document.addEventListener('mousemove', onDocumentMouseMove, false);
+
+        function onDocumentMouseMove(event) {
             event.preventDefault();
-            mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-            mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+            mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+            mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
             planet.setMouse(mouse);
             var lalo = planet.pointerLoLa;
-            if(lalo.valid)
-            latlon.innerHTML='['+lalo.loF+','+lalo.laF+']'+'['+lalo.lo+','+lalo.la+']';
+            if (lalo.valid)
+                latlon.innerHTML = '[' + lalo.loF + ',' + lalo.laF + ']' + '[' + lalo.lo + ',' + lalo.la + ']';
             else
-            latlon.innerHTML= '';
+                latlon.innerHTML = '';
             //console.log(mouse);
         }
         //地图加载
         mapLoad();
         var gui = new dat.GUI();
-        var textureGui = gui.addFolder( 'texture' );
-        textureGui.add( planet.params, 'lolaLinesShow' );
-        textureGui.add( planet.params, 'cloudShow' );
-        var contorlGui = gui.addFolder( 'control' );
-        contorlGui.add( planet.params, 'autoRotate' );
-        
-        gui.add( paramC, 'RayMarching' ).onChange(paramOnChange);
+        var textureGui = gui.addFolder('texture');
+        textureGui.add(planet.params, 'lolaLinesShow');
+        textureGui.add(planet.params, 'cloudShow');
+        var contorlGui = gui.addFolder('control');
+        contorlGui.add(planet.params, 'autoRotate');
+
+        gui.add(paramC, 'RayMarching').onChange(paramOnChange);
         //planet.setParams(params);
         gui.open();
-        for(var i = 0;i<50;i++){
-            var s1=Math.random()-0.5;
-            var s2=Math.random()-0.5;
-            var e1 = Math.random()-0.5;
-            var e2 = Math.random()-0.5;
-            planet.addSingleLine(new planet.LoLa(s1*360,s2*180),new planet.LoLa(113.59,22.35),0xff0000);
-         }
-         planet.addSingleLine(new planet.LoLa(30,10),new planet.LoLa(100,-10),0xffff00);
+        for (let i = 0; i < 50; i++) {
+            let s1 = Math.random() - 0.5;
+            let s2 = Math.random() - 0.5;
+            let e1 = Math.random() - 0.5;
+            let e2 = Math.random() - 0.5;
+            planet.addSingleLine(new planet.LoLa(s1 * 360, s2 * 180), new planet.LoLa(113.59, 22.35), 0xffffff);
+        }
+        for (let i = 0; i < 50; i++) {
+            let s1 = Math.random() - 0.5;
+            let s2 = Math.random() - 0.5;
+            let e1 = Math.random() - 0.5;
+            let e2 = Math.random() - 0.5;
+            planet.addSingleLine(new planet.LoLa(s1 * 360, s2 * 180), new planet.LoLa(-100, -50), 0xffff00);
+        }
+        //planet.addSingleLine(new planet.LoLa(30, 10), new planet.LoLa(100, -10), 0xffff00);
         // var cdsac = new THREE.BoxGeometry(0.01,0.01,0.01 );
-		// var cdaca = new THREE.MeshBasicMaterial( {wireframe:true} );
+        // var cdaca = new THREE.MeshBasicMaterial( {wireframe:true} );
         // var lons = 100,lats = 0;
         // for(var i = 0;i<50;i++){
         //     for(var j = 0;j<40;j++){
@@ -68,42 +77,45 @@ var Contorl = function(){
 
 
     }
-    function mapLoad(){
-        var filename = "china-provinces.json";//world-50m
-        $.getJSON('maps/'+filename,function(topodata){
+
+    function mapLoad() {
+        var filename = "china-provinces.json"; //world-50m
+        $.getJSON('maps/' + filename, function(topodata) {
             var o = topodata.objects;
-            var temp = topojson.feature(topodata,o.CHN_adm1); 
+            var temp = topojson.feature(topodata, o.CHN_adm1);
             console.log(temp);
-            planet.addMap(temp,"ChinaMap");
+            planet.addMap(temp, "ChinaMap");
         });
-        filename = "world-countries.json";//world-50m
-        $.getJSON('maps/'+filename,function(topodata){
+        filename = "world-countries.json"; //world-50m
+        $.getJSON('maps/' + filename, function(topodata) {
             var o = topodata.objects;
-            var temp = topojson.feature(topodata,o.countries1); 
+            var temp = topojson.feature(topodata, o.countries1);
             console.log(temp);
-            planet.addMap(temp,"worldMap");
+            planet.addMap(temp, "worldMap");
         });
     }
-    function paramOnChange(){
-        if(paramC.RayMarching)
+
+    function paramOnChange() {
+        if (paramC.RayMarching)
             rayMarchPross();
-        else{
+        else {
             planet.romoveMesh(objName.rayMarch)
             planet.removeRenderTarget(objName.rayMarch);
         }
     }
-    function rayMarchPross(){
+
+    function rayMarchPross() {
         var filename = "data.json";
-        $.getJSON('data/'+filename,function(data){
-            var rayMarch = new  RayMarching(data);
+        $.getJSON('data/' + filename, function(data) {
+            var rayMarch = new RayMarching(data);
             var mesh = rayMarch.mesh;
             var meshRT = rayMarch.meshRT;
-            mesh.scale.set(0.4,0.4,0.4);
-            meshRT.scale.set(0.4,0.4,0.4);
-            planet.addMesh(mesh,objName.rayMarch,new planet.LoLa(117.5,22.35),0.3);
-            planet.addMeshToRT(meshRT,objName.rayMarch,new planet.LoLa(117.5,22.35),0.3);
-            planet.addRenderTarget(rayMarch.rtObj,objName.rayMarch);      
-        }); 
+            mesh.scale.set(0.4, 0.4, 0.4);
+            meshRT.scale.set(0.4, 0.4, 0.4);
+            planet.addMesh(mesh, objName.rayMarch, new planet.LoLa(117.5, 22.35), 0.3);
+            planet.addMeshToRT(meshRT, objName.rayMarch, new planet.LoLa(117.5, 22.35), 0.3);
+            planet.addRenderTarget(rayMarch.rtObj, objName.rayMarch);
+        });
     }
 
     //console.log(planet);
