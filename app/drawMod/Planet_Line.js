38b222175572641607fe9fbd36d07ffee3c5bc87
DOGTT <bolitt@hotmail.com>
2017-08-30 23:15:03 +0800

    m

--- app/drawMod/Planet_Line.js
+++ app/drawMod/Planet_Line.js
@@ -1,3 +1,6 @@
-var drawLine = function() {
-
+this.addSingleLine = function(lolaS, lolaE, color) {
+    var color = color !== undefined ? color : 0xffffff;
+    if (lolaS instanceof PlanetLoLa && lolaE instanceof PlanetLoLa) {
+        addLine(lolaS, lolaE, color, true);
+    }
 };
--- app/planet.js
+++ app/planet.js
@@ -217,37 +217,6 @@ var Planet = function(configP, domElement) {
 
     }
 
-    function addLine(p1, p2, color, dynamic) {
-        var p1 = _this.LoLaconvertToXYZ(p1);
-        var p2 = _this.LoLaconvertToXYZ(p2);
-        var high = 0.4,
-            pointMount = 40;
-        var linePoints = PointToPoint(p1, p2, high, pointMount);
-
-        var geometry = new THREE.BufferGeometry();
-        var positions = new Float32Array(pointMount * 3);
-        geometry.addAttribute('position', new THREE.BufferAttribute(
-            positions, 3
-        ));
-        for (let i = 0; i < pointMount; i++) {
-            positions[i * 3] = linePoints[i].x;
-            positions[i * 3 + 1] = linePoints[i].y;
-            positions[i * 3 + 2] = linePoints[i].z;
-        }
-        if (dynamic) {
-
-        }
-        var material = new THREE.LineBasicMaterial({
-            color: color,
-            linewidth: 2
-        });
-
-        //console.log(linesGroup);
-        var line = new THREE.Line(geometry, material);
-        line.dynamic = dynamic;
-        linesGroup.add(line);
-    }
-
     function makeAmbient() {
         makeUniverse();
         makeLoLaLine();
@@ -752,12 +721,7 @@ var Planet = function(configP, domElement) {
         var name = name !== undefined ? name : DefaultMeshName;
         this.renderTargets[name] = rt;
     };
-    this.addSingleLine = function(lolaS, lolaE, color) {
-        var color = color !== undefined ? color : 0xffffff;
-        if (lolaS instanceof PlanetLoLa && lolaE instanceof PlanetLoLa) {
-            addLine(lolaS, lolaE, color, true);
-        }
-    };
+
     this.addAnimationLines = function(pointSArray, pointEArray) {
         if (pointSArray.length === pointEArray.length) {
             for (let i = 0; i < pointEArray.length; i++) {
