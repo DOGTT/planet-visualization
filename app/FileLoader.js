4025073f6a924be698c17b33947cd3938ab480da
DOGTT <bolitt@hotmail.com>
2017-09-08 16:00:34 +0800

    m

--- app/FileLoader.js
+++ app/FileLoader.js
@@ -1,45 +1,35 @@
-(function (global,factory){
-    typeof exports === 'object' && typeof module!=='undefined'?module.exports=factory():
-    typeof define === 'function' && define.amd?define(factory):(global.FileLoader=factory());   
-}(this,(function(){
-    
-'use strict';
 /**
  * @author tide_h 
  * loader need fixed
  * 
  * 
  */
-    function FileLoader(){
-        var vShader = null;
-        var fShader= null;
-        var path = 'app/shader_lib/';
-        return {
-            loadShader:function(name){     
-                return new Promise((resolve,reject)=>{
-                    if(name.length<2) return;
-                    $.get(path+name[0],function(vs){
-                        $.get(path+name[1],function(fs){
-                            fShader = fs;
-                            vShader = vs;
-                            resolve(name);
-                        });
+function FileLoader() {
+    'use strict';
+    var vShader = null;
+    var fShader = null;
+    var path = 'app/shader_lib/';
+    return {
+        loadShader: function(name) {
+            return new Promise((resolve, reject) => {
+                if (name.length < 2) return;
+                $.get(path + name[0], function(vs) {
+                    $.get(path + name[1], function(fs) {
+                        fShader = fs;
+                        vShader = vs;
+                        resolve(name);
                     });
                 });
-            },
-            setPath:function(p){
-                path=p;
-            },
-            getvShader:function(){
-                return vShader;
-            },
-            getfShader:function(){
-                return fShader;
-            }
+            });
+        },
+        setPath: function(p) {
+            path = p;
+        },
+        getvShader: function() {
+            return vShader;
+        },
+        getfShader: function() {
+            return fShader;
         }
-    }
-    //Object.assign(FileLoader.prototype,);
-    return FileLoader;
-    // exports.FileLoader = FileLoader;
-    // Object.defineProperty(exports, '__esModule', { value: true });
-})));
+    };
+}
